8526e674ddbffa25aea88e204dd34bba285ed8d5
David Enamorado <davidenamorado@Davids-MacBook-Pro.local>
2024-07-02 00:05:31 -0300

    deploy vercel test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/myapp/',
+  base: '/jpower-landing.vercel.app/',
   plugins: [vue()],
 })
